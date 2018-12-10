b4e51cd89dfaf28d2b0879ee5791de79730dbe25
Jon Walls <jonathan.walls@looker.com>
2018-12-10 22:30:01 +0000

    Working version of HTML Tile

--- html_tile/html_tile.js
+++ html_tile/html_tile.js
@@ -1,3 +1,10 @@
+html_tile_style = `
+  .html-tile {
+    font-family: Open Sans,Helvetica,Arial,sans-serif;
+    font-size: 12px;   
+  }
+`
+
 looker.plugins.visualizations.add({
   options: {
     html_template: {
@@ -9,63 +16,37 @@ looker.plugins.visualizations.add({
   },
 
   create: function(element, config) {
-
-    // Insert a <style> tag with some styles we'll use later.
-    var css = element.innerHTML = `
-      <style>
-        .html-tile {
-          // Vertical centering
-          height: 100%;
-          display: flex;
-          flex-direction: column;
-        }
-
-        .html-tile-text-small {
-          font-size: 12px;
-        }
-      </style>
-    `;
+    this.style = document.createElement('style')
+    document.head.appendChild(this.style)
 
     // Create a container element to let us center the text.
-    var container = element.appendChild(document.createElement("div"));
-    container.className = "hello-world-vis";
-
-    // Create an element to contain the text.
-    this._htmlTile = container.appendChild(document.createElement("div"));
-    this._htmlTile.className = "html-tile-text-small";
+    this.container = element.appendChild(document.createElement("div"));
+    this.container.className = "html-tile";
 
   },
   updateAsync: function(data, element, config, queryResponse, details, done) {
-    var engine = new Liquid();
-    var template = `<h3>Total Sale Price is {{ order_items_total_sale_price }}</h3>`
-
     // Clear any errors from previous updates.
     this.clearErrors();
 
-    // print data to console for debugging:
-    console.log("data",data);
-    console.log("config",config);
-    console.log("queryResponse",queryResponse);
-    console.log("element", element);
-
-    // Grab the first cell of the data.
+    // Grab the first row of the data.
     var firstRow = data[0];
-    console.log("firstRow", firstRow);
-    for (var measure in firstRow) {
-      console.log("measure: ", measure)
-    };
+
+    var engine = new Liquid();
+    tpl = engine.parse(config.html_template);
 
     parameters = {};
-    parameters["order_items_total_sale_price"] = firstRow["order_items.total_sale_price"].rendered;
-    console.log("parameters", parameters)
+    for (var j = 0; j < tpl.length; j++) {
+      var tag = tpl[j]
+      if (tag.type == "value") {
+        raw_name = tag.initial.replace("--", ".");
+        parameters[tag.initial] = firstRow[raw_name].rendered
+      }
+    }
 
-    output = engine
-        .parseAndRender(template, parameters)
-        .then(html => this._htmlTile.innerHTML = html)
-
-    // Insert the data into the page.
-    // this._htmlTile.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
-    // this._htmlTile.innerHTML = output // config.html_template;
+    this.style.innerHTML = html_tile_style;
+    engine
+        .render(tpl, parameters)
+        .then(html => this.container.innerHTML = html);
 
     // Always call done to indicate a visualization has finished rendering.
     done()
