c0caccdfda3e482ffad336190ca2bcd532d15c1a
Jon Walls <jonathan.walls@looker.com>
2019-05-13 09:41:56 +0100

    redirected to use modified css

--- leaflet_images/leaflet_image_overlay.js
+++ leaflet_images/leaflet_image_overlay.js
@@ -115,7 +115,7 @@ const vis = {
         var csslink  = document.createElement('link');
         csslink.rel  = 'stylesheet';
         csslink.type = 'text/css';
-        csslink.href = 'https://unpkg.com/leaflet@1.5.1/dist/leaflet.css';
+        csslink.href = 'https://raw.githack.com/ContrastingSounds/Looker-Custom-Vis/leaflet_images_dev/leaflet_images/leaflet_image_overlay.css';
         csslink.crossorigin = "";
         document.head.appendChild(csslink);
 
