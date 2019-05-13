4bbf38867f65c372fd7010bc50ef1221eb39fe32
Jon Walls <jonathan.walls@looker.com>
2019-05-13 15:53:48 +0100

    geojson zones

--- mapbox/custom_mapbox.js
+++ mapbox/custom_mapbox.js
@@ -1,5 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9ud2FsbHMiLCJhIjoiY2p2aTZsbnh4MDJrbjRibWcxZ2UydXhiayJ9.n25D6UcjxzRQsuKiY5un8A';
 
+const dumpToConsole = function(message, obj) {
+    console.log(message, JSON.stringify(obj, null, 2));
+}
+
 const default_options = {
   // Map Options
   mapStyle: {
@@ -13,6 +17,11 @@ const default_options = {
       {"Dark": "dark"},
       {"Outdoor": "outdoor"},
       {"Satellite": "satellite"},
+      {"Le Shine": "le_shine"},
+      {"Moonlight": "moonlight"},
+      {"Decimal Dark": "decimal"},
+      {"North Star": "north_star"},
+      {"Minimo": "minimo"},
     ],
     default: "streets",
   },
@@ -33,6 +42,21 @@ const map_options = {
     },
     'satellite': {
         'style': 'mapbox://styles/mapbox/satellite-v9'
+    },
+    'le_shine': {
+        'style': 'mapbox://styles/jonwalls/cjvmcxx3w07kf1cpgkj7kqbxa'
+    },
+    'moonlight': {
+        'style': 'mapbox://styles/jonwalls/cjvmd478d03td1clm08k2egzq'
+    },
+    'decimal': {
+        'style': 'mapbox://styles/jonwalls/cjvmd7k1m1uzg1ckjssqtnqnf'
+    },
+    'north_star': {
+        'style': 'mapbox://styles/jonwalls/cjvmd9w2d3xae1clzg192pdqj'
+    },
+    'minimo': {
+        'style': 'mapbox://styles/jonwalls/cjvmdchkn3xjl1bo3ulk8wv0d'
     },
 }
 
@@ -58,7 +82,7 @@ const vis = {
     updateAsync: function(data, element, config, queryResponse, details, done) {
         this.clearErrors();
 
-        console.log(JSON.stringify(queryResponse, null, 2));
+        // console.log(JSON.stringify(queryResponse, null, 2));
 
         const chartHeight = element.clientHeight - 16;
         const dimensions = queryResponse.fields.dimension_like;
@@ -85,31 +109,62 @@ const vis = {
         map_element.id = "map";
         map_element.setAttribute("style","height: 100%");  // " + chartHeight + "px");
 
-        // var map = L.map('leafletMap');
-        
-        // L.tileLayer(
-        //     map_options[config.mapStyle].tiles_url, 
-        //     map_options[config.mapStyle].metadata
-        // ).addTo(map);
-
         var map = new mapboxgl.Map({
             container: 'map', // container id
             style: map_options[config.mapStyle].style, // stylesheet location
-            center: [-74.50, 40], // starting position [lng, lat]
+            center: [-74.01, 40.71], // starting position [lng, lat]
             zoom: 9 // starting zoom
         });
 
         // As an alternative to Looker have a location type,
         // we can use tags (e.g. "geojson") in LookML
-        // var geoLayer = L.geoJSON().addTo(map);
-        // for (let d = 0; d < dimensions.length; d++) {
-        //     if (dimensions[d].tags.includes("geojson")) {
-        //         for (let row = 0; row < data.length; row++) {
-        //             geojson_value = JSON.parse(data[row][dimensions[d].name].value);
-        //             geoLayer.addData(geojson_value);
-        //         }                
-        //     }
-        // }
+        map.on('load', function() {
+            for (let d = 0; d < dimensions.length; d++) {
+                if (dimensions[d].tags.includes("geojson")) {
+                    // let source_name = dimensions[d].name+"-source";
+                    // let map_source = {
+                    //     "type": "geojson",
+                    //     "data": {
+                    //         "type": "FeatureCollection",
+                    //         "features": []
+                    //     }
+                    // }
+                    // map.addSource(source_name, map_source);
+                    // console.log("source_name", source_name);
+
+                    for (let row = 0; row < data.length; row++) {
+                        let geojson_value = JSON.parse(data[row][dimensions[d].name].value);
+                        // let feature = {
+                        //    "type": "Feature",
+                        //    "geometry": geojson_value
+                        // }
+                        // map_source.data.features.push(feature);
+                        // console.log("push feature...")
+                        map.addLayer({
+                            "id": dimensions[d].name + "-layer" + d + row,
+                            "type": "fill",
+                            "source": {
+                                "type": "geojson",
+                                "data": geojson_value,
+                            },
+                            "layout": {},
+                            "paint": {
+                                "fill-color": "#ff0000",
+                                "fill-opacity": 0.4
+                            }
+                        }); 
+                    }
+                    // console.log("map_source.data.features", map_source.data.features)
+                    // dumpToConsole("map_source", map_source);
+                    // let layer_name = dimensions[d].name + "-layer";
+
+                    // console.log("layer_name", layer_name);
+                    // console.log("source_name", source_name);        
+                }
+            }
+            console.log(map);
+        });
+
         // map.fitBounds(geoLayer.getBounds());
 
         done();
